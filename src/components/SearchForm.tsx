87fe99979ae91220305d753d460d932d96971254
akshaymalik1995 <akshaymalik191@gmail.com>
2024-06-08 04:07:28 +0530

    Suggestion Box Removed

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SuggestionsList from "./SuggestionsList";
 import { useContext } from "react";
-import store from "../app/store";
 import StoreContext from "../app/store";
 import { updateSearchWeather } from "../app/actions";
 import { IWeatherData } from "../app/types";
@@ -11,19 +10,13 @@ async function getCitiesSuggestion(value: string) {
     const url = `https://api.thecompaniesapi.com/v1/locations/cities?search=${encodeURIComponent(
       value
     )}&size=5`;
+    
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error("Failed to get cities" + response.statusText);
     }
     const data = await response.json();
-    const cities = data.cities.reduce(
-      (list: string[], item: { [name: string]: string }) => [
-        ...list,
-        item.name,
-      ],
-      []
-    );
-    return cities;
+    return data.cities
   } catch (error) {
     console.error(error);
     return [];
@@ -31,8 +24,9 @@ async function getCitiesSuggestion(value: string) {
 }
 
 async function getWeather(value: string) {
-    try {
-        const OPEN_WEATHER_KEY = import.meta.env.VITE_OPEN_WEATHER_KEY;
+  try {
+    console.debug(value)
+    const OPEN_WEATHER_KEY = import.meta.env.VITE_OPEN_WEATHER_KEY;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
       value
     )}&appid=${OPEN_WEATHER_KEY}&units=metric`;
@@ -41,62 +35,94 @@ async function getWeather(value: string) {
       throw new Error("Failed to get weather." + response.statusText);
     }
     const data = await response.json();
-    return data
-    
+    return data;
   } catch (error) {
     console.error(error);
   }
 }
 
 export default function SearchForm() {
-  const [globalState, dispatch] = useContext(StoreContext)
+  const [globalState, dispatch] = useContext(StoreContext);
   const [locationInput, setLocationInput] = useState("");
-  const [citySuggestions, setCitySuggestions] = useState([]);
+  const [citySuggestions, setCitySuggestions] = useState<Object[]>([]);
+  const [suggestionsOpen, setSuggestionsOpen] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("click", closeSuggestions);
+
+    return () => {
+      document.removeEventListener("click", closeSuggestions)
+    }
+  }, [])
 
   function onSuggestionSelection(value: string) {
     setLocationInput(value);
-    }
-    
-    async function onFormSubmit(e: React.FormEvent) {
-        e.preventDefault()
-      const weatherData = await getWeather(locationInput)
-      const newWeatherData: IWeatherData = {
-        location: weatherData.name,
-        minTemp: weatherData.main.temp_min,
-        maxTemp: weatherData.main.temp_max,
-        temperature: weatherData.main.temp,
-        weather : weatherData.weather[0].main
-      };
-      dispatch(updateSearchWeather(newWeatherData))
-    }
+    closeSuggestions();
+    displaySearchWeatherData(value)
+  }
+
+  async function displaySearchWeatherData(value : string) {
+    const weatherData = await getWeather(value);
+    const newWeatherData: IWeatherData = {
+      location: weatherData.name,
+      minTemp: weatherData.main.temp_min,
+      maxTemp: weatherData.main.temp_max,
+      temperature: weatherData.main.temp,
+      weather: weatherData.weather[0].main,
+    };
+    dispatch(updateSearchWeather({...newWeatherData}));
+  }
+
+  function onFormSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    displaySearchWeatherData(locationInput)
+  }
+
+  function closeSuggestions() {
+    setSuggestionsOpen(false);
+  }
+
+  function openSuggestions() {
+    setSuggestionsOpen(true);
+  }
 
   async function handleInputChange(value: string) {
-    
     setLocationInput(value);
     if (!value) {
+      closeSuggestions();
       setCitySuggestions([]);
       return;
     }
-    const cities = await getCitiesSuggestion(value);
-    setCitySuggestions(cities);
+    openSuggestions();
+    const cities : Object[] = await getCitiesSuggestion(value);
+    if (cities) {
+      setCitySuggestions([...cities]);
+    }
+    
   }
   return (
     <div className="w-full">
       {/* A Form to take user input - Location */}
-      <form onSubmit={e => onFormSubmit(e)} className="flex w-full gap-4 " action="">
+      <form
+        onSubmit={(e) => onFormSubmit(e)}
+        className="flex w-full gap-4 "
+        action=""
+      >
         {/* A Text Input */}
         <div className="grow relative">
           <input
+            
             value={locationInput}
             onChange={(e) => handleInputChange(e.target.value)}
             autoFocus={true}
             placeholder="Search City.."
             className="px-2 w-full py-2 shadow focus:outline-none "
-            type="text"
+            type="search"
           />
           <SuggestionsList
+            isOpen={suggestionsOpen}
             onSuggestionSelection={onSuggestionSelection}
-            suggestions={citySuggestions}
+            suggestions={[...citySuggestions]}
           />
         </div>
         {/* Button to submit the form */}
--- src/components/SuggestionsList.tsx
+++ src/components/SuggestionsList.tsx
@@ -1,16 +1,33 @@
 import { CiSearch } from "react-icons/ci";
 
-export default function SuggestionsList(props: { suggestions: string[] , onSuggestionSelection : (value : string) => void}) {
-  const cities = props.suggestions;
+export default function SuggestionsList(props: {
+  suggestions: Object[];
+  onSuggestionSelection: (value: string) => void;
+  isOpen: Boolean;
+}) {
+
+  function handleClick(e : React.MouseEvent, value : string){
+    // e.stopPropagation()
+    props.onSuggestionSelection(value)
+  }
+  
   return (
-    <div className="shadow bg-white flex rounded flex-col w-full absolute top-full">
-      {cities.length > 0 &&
-        cities.map((city) => (
-          <div onClick={() => props.onSuggestionSelection(city)} className="px-2 py-2 hover:bg-gray-100  cursor-pointer  flex items-center gap-2">
+    <div
+      className={`shadow ${
+        props.isOpen ? "" : "hidden"
+      } bg-white flex rounded flex-col w-full absolute top-full`}
+    >
+      {props.suggestions.length > 0 &&
+        props.suggestions.map((city) => (
+          <div
+            key={city.code}
+            onClick={(e) => handleClick(e, city.name)}
+            className="px-2 py-2 hover:bg-gray-100  cursor-pointer  flex items-center gap-2"
+          >
             <div className="text-lg text-gray-500  ">
               <CiSearch />
             </div>
-            <div className="text-gray-700" >{city}</div>
+            <div className="text-gray-700">{city.name}, { city.country.name }</div>
           </div>
         ))}
     </div>
